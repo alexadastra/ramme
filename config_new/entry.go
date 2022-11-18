475798bc385299ea9c9340f3198d3d64fd0479e4
alexadastra <semenchenkoaleksey92@gmail.com>
2022-11-18 21:23:11 +0300

    add new config storage

---
+++ config_new/entry.go
@@ -0,0 +1,31 @@
+package config_new
+
+import "time"
+
+// Entry represents some value that was passed in config
+type Entry interface{}
+
+// ToInt converts entry to int
+func ToInt(e Entry) int {
+	return e.(int)
+}
+
+// ToString converts entry to string
+func ToString(e Entry) string {
+	return e.(string)
+}
+
+// ToBool converts entry to bool
+func ToBool(e Entry) bool {
+	return e.(bool)
+}
+
+// ToDuration converts entry to Duration
+func ToDuration(e Entry) time.Duration {
+	return e.(time.Duration)
+}
+
+// ToUInt converts entry to uint
+func ToUInt(e Entry) uint {
+	return e.(uint)
+}
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/sirupsen/logrus v1.9.0
 	gopkg.in/fsnotify.v1 v1.4.7
 	gopkg.in/yaml.v2 v2.4.0
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c
 )
 
 require (
